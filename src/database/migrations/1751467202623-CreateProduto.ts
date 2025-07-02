30dfbd2c33d8661df204e7bac249dd13ee0fa7ab
lucas joel <lucas.joel1628@gmail.com>
2025-07-02 11:48:21 -0300

    aprendisado de database

---
+++ src/database/migrations/1751467202623-CreateProduto.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateProduto1751467202623 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "produtos",
+                columns : [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment"
+                    }
+                ]
+
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
