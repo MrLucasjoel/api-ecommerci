c84d955e20ff993fe327d209c527db43b45b9898
lucas joel <lucas.joel1628@gmail.com>
2025-07-14 08:29:23 -0300

    alteração

--- src/index.ts
+++ src/index.ts
@@ -32,8 +32,8 @@ AppDataSource.initialize()
         app.use('/produtos', routeProduto)
         app.use('/usuarios', routeUser)
         app.use('/login', routerLogin)
-        app.listen(process.env.API_PORT, () => {
-            console.log("servidor rodando na porta ", process.env.API_PORT);
+        app.listen(process.env.PORT, () => {
+            console.log("servidor rodando na porta ", process.env.PORT);
         })
         console.log("Conexão com o banco de dados estabelecida com sucesso!");
     });
