a9f668b7018f40ea7172d2bf7715b95b21f48ada
lucas joel <lucas.joel1628@gmail.com>
2025-07-14 09:21:10 -0300

    alterações de token

--- src/routes/usuario.routes.ts
+++ src/routes/usuario.routes.ts
@@ -4,10 +4,10 @@ import { Usuario } from "../entities/Usuario";
 import { authJwt } from "../middlewares/authJwt";
 
 const routes = Router()
-routes.get("/",authJwt ,UsuarioController.getAll)
-routes.get("/:id", UsuarioController.getOne)
-routes.post("/", UsuarioController.create)
-routes.put("/:id", UsuarioController.update)
-routes.delete("/:id", UsuarioController.delete)
+routes.get("/", authJwt ,UsuarioController.getAll)
+routes.get("/:id", authJwt, UsuarioController.getOne)
+routes.post("/", authJwt, UsuarioController.create)
+routes.put("/:id", authJwt, UsuarioController.update)
+routes.delete("/:id", authJwt, UsuarioController.delete)
 
 export default routes
